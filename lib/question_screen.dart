12e102ab9adff8c2dadaf874ae6cb5696c645b02
burakborhan <borhanburak@hotmail.com>
2023-12-20 12:42:43 +0300

    app done

---
+++ lib/question_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({super.key, required this.onSelectedAnswer});
+  final void Function(String answer) onSelectedAnswer;
+
+  @override
+  State<QuestionScreen> createState() => _QuestionScreenState();
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+  List<String> selectedAnswers = [];
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectedAnswer(selectedAnswer);
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                  color: Color.fromARGB(255, 201, 153, 251),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map(
+              (answer) {
+                return AnswerButton(
+                    answerText: answer,
+                    onTap: () {
+                      answerQuestion(answer);
+                    });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
